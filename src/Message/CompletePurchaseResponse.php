4fdfbbf66f7d97765ebb3429fc9eb801243396af
Andrew Coates <andycoatz@gmail.com>
2015-07-20 12:35:15 +0100

    Added optional confirm method
    This allows us to return the customer to our own domain, following a transaction.

--- src/Message/CompletePurchaseResponse.php
+++ src/Message/CompletePurchaseResponse.php
@@ -28,4 +28,22 @@ class CompletePurchaseResponse extends AbstractResponse
     {
         return isset($this->data['rawAuthMessage']) ? $this->data['rawAuthMessage'] : null;
     }
+    
+    /**
+     * Optional step: Redirect the customer back to your own domain.
+     *
+     * This is achieved by returning a HTML string containing a meta-redirect which is displayed by WorldPay
+     * to the customer. This is far from ideal, but apparently (according to their support) this is the only
+     * method currently available.
+     *
+     * @param string $returnUrl The URL to forward the customer to.
+     * @param string $message   Optional message to display to the customer before they are redirected.
+     */
+    public function confirm(
+        $returnUrl,
+        $message = 'Thank you, your transaction has been processed. You are being redirected...'
+    ) {
+        echo '<meta http-equiv="refresh" content="2;url='.$returnUrl.'" /><p>'.$message.'</p>';
+        exit;
+    }
 }
