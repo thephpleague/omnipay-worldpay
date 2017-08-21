87a2d03c32b713b4bacf6cb32093565f5f00f34c
dpfaffenbauer <dominik@lineofcode.at>
2017-08-21 15:07:57 +0200

    make worldpay compatible with omnipay 3.*

--- src/Message/JsonAbstractRequest.php
+++ src/Message/JsonAbstractRequest.php
@@ -43,36 +43,27 @@ abstract class JsonAbstractRequest extends AbstractRequest
         return $this->setParameter('clientKey', $value);
     }
 
+    /**
+     * Method required to override for getting the specific request endpoint
+     *
+     * @return string
+     */
+    abstract public function getEndpoint();
 
     public function sendRequest($data)
     {
-        $config = $this->httpClient->getConfig();
-        $curlOptions = $config->get('curl.options');
-        $curlOptions[CURLOPT_SSLVERSION] = 6;
-        $config->set('curl.options', $curlOptions);
-        $this->httpClient->setConfig($config);
-
-        // don't throw exceptions for 4xx errors
-        $this->httpClient->getEventDispatcher()->addListener(
-            'request.error',
-            function ($event) {
-                if ($event['response']->isClientError()) {
-                    $event->stopPropagation();
-                }
-            }
-        );
-
         $httpRequest = $this->httpClient->createRequest(
             $this->getHttpMethod(),
             $this->getEndpoint(),
-            null,
+            [],
             json_encode($data)
         );
 
-        $httpResponse = $httpRequest
-            ->setHeader('Authorization', $this->getServiceKey())
-            ->setHeader('Content-type', 'application/json')
-            ->send();
+        $httpRequest = $httpRequest
+            ->withHeader('Authorization', $this->getServiceKey())
+            ->withHeader('Content-type', 'application/json');
+
+        $httpResponse = $this->httpClient->sendRequest($httpRequest);
 
         return $httpResponse;
     }
--- src/Message/JsonPurchaseRequest.php
+++ src/Message/JsonPurchaseRequest.php
@@ -50,7 +50,6 @@ class JsonPurchaseRequest extends JsonAbstractRequest
             $data['deliveryAddress']['telephoneNumber'] = $card->getBillingPhone();
 
             $data['shopperEmailAddress'] = $card->getEmail();
-
         }
 
         $data['shopperIpAddress'] = $this->getClientIp();
